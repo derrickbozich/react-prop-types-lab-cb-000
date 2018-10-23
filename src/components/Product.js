affa1e246c37478be7987fb50c019689e04f7255
derrick b <dbozich1@gmail.com>
2018-10-23 00:22:15 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,3 +4,12 @@ import React from 'react'
 export default class Product extends React.Component{
   
 }
+
+Product.propTypes = {
+  name: PropTypes.string,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
