0079cdb5011064720727ca0d6bc3b508d2c1d78f
derrick b <dbozich1@gmail.com>
2018-10-23 00:36:03 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,6 +9,20 @@ Product.propTypes = {
   name: PropTypes.string,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
-  color: PropTypes.string.isRequired
-  weight: PropTypes.number.isRequired
+  color: PropTypes.string.isRequired,
+  weight: weightCheck
+}
+
+function weightCheck(props, propName, componentName) {
+  componentName = comopnentName || 'ANONYMOUS';
+
+  if (props[propName]) {
+    let value = props[propName];
+    if (typeof value === 'string') {
+        return value.length <= 140 ? null : new Error(propName + ' in ' + componentName + " is longer than 140 characters");
+    }
+  }
+
+  // assume all ok
+  return null;
 }
